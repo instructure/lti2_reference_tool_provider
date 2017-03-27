68b7ad326efa28b48cc4fac704e8982c144567c7
Nathan Mills <nathanm@instructure.com>
2017-03-27 13:32:35 -0600

    fix rubocop error

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require 'bundler/setup'
 Bundler.require(:default)
 Bundler.require(Sinatra::Base.environment)
 
-ActiveRecord::Base.dump_schema_after_migration= false if ENV['RACK_ENV'] == 'production'
+ActiveRecord::Base.dump_schema_after_migration = false if ENV['RACK_ENV'] == 'production'
 
 set :database_file, ENV['DATABASE_CONFIG'] || 'default_database.yml'
 
