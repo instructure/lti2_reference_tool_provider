f91044d90f155e565d3770d131d191f23e368346
Nathan Mills <nathanm@instructure.com>
2017-03-29 13:16:22 -0600

    add coveralls support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 ENV['RACK_ENV'] = 'test'
 require_relative '../config/environment'
+require 'coveralls'
+Coveralls.wear
 
 RSpec.shared_context 'spec_helper', shared_context: :metadata do
   RSpec.configure do |config|
